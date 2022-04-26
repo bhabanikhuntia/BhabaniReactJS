b502cbe313e80c6cfcf7387ffb3c8ccb54b787bd
bhabanikhuntia <96862165+bhabanikhuntia@users.noreply.github.com>
2022-04-26 10:34:01 +0530

    update

--- SyswayReact/src/Pages/Home/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Welcome() {
-  return (
-    <div>
-    <h1 style={{color:"blue", textAlign:"center", marginTop:"8rem"}}>WELCOME TO THE WORLD OF REACT</h1>
-    </div>
-  )
-}
